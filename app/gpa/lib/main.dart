b81650015154daaabe9fa5cbe3826dfc863f4c38
GauravAnand563 <gauravanand563@gmail.com>
2020-10-14 00:30:21 +0530

    implemented the appbar of HomePage

--- app/gpa/lib/main.dart
+++ app/gpa/lib/main.dart
@@ -1,28 +1,30 @@
 import 'package:flutter/material.dart';
 import './ui/pages/home.dart';
 import './ui/pages/result.dart';
+import 'package:flutter/services.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  
   @override
   Widget build(BuildContext context) {
+    SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
+      statusBarColor: Color(0xffcabbf9), //or set color with: Color(0xFF0000FF)
+    ));
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        primarySwatch: Colors.pink,
-        visualDensity: VisualDensity.adaptivePlatformDensity,
+        primaryColor: Color(0xffcabbe9),
+        accentColor: Color(0xffffcef3),
       ),
       // home: HomePage(),
       initialRoute: HomePage.id,
       routes: {
         ResultsPage.id: (context) => ResultsPage(),
-        HomePage.id : (context) => HomePage(),
+        HomePage.id: (context) => HomePage(),
       },
     );
   }
 }
-
--- app/gpa/lib/ui/pages/home.dart
+++ app/gpa/lib/ui/pages/home.dart
@@ -8,51 +8,71 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  double isScrolled = 0.0;
+  final controller = ScrollController();
+  Widget wi = BigAvatar();
+  Animation<double> animation;
+
+  @override
+  void initState() {
+    super.initState();
+    controller.addListener(onScroll);
+  }
+
+  @override
+  void dispose() {
+    controller.removeListener(onScroll);
+  }
+
+  onScroll() {
+    setState(() {
+      if (wi is BigAvatar) if (controller.offset /
+              controller.position.maxScrollExtent >=
+          0.3) wi = SmallAvatar();
+      if (wi is SmallAvatar) if (controller.offset /
+              controller.position.maxScrollExtent <=
+          0.7) wi = BigAvatar();
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: CustomScrollView(
-        controller: ScrollController(
-          
-        ),
-        slivers: [
+      backgroundColor: Colors.white,
+      body: CustomScrollView(controller: controller, slivers: [
         SliverAppBar(
-          backgroundColor: Colors.blueGrey[900],
-          expandedHeight: 200.0,
+          elevation: 0,
+          backgroundColor: Colors.white,
+          expandedHeight: 190,
           floating: true,
           pinned: true,
-          flexibleSpace: Column(
+          flexibleSpace: Row(
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
               Container(
                 margin: EdgeInsets.only(top: 25),
-                padding: EdgeInsets.symmetric(vertical :15),
-                height: 150,
-                width: double.infinity,
-                child: Neumorphic(
-                  padding: EdgeInsets.all(12),
-                  style: NeumorphicStyle(
-                      boxShape: NeumorphicBoxShape.circle(),
-                      depth: 20,
-                      lightSource: LightSource.bottomRight,
-                      shadowDarkColor: Colors.black26),
-                  child: Neumorphic(
-                      style: NeumorphicStyle(
-                          boxShape: NeumorphicBoxShape.circle(),
-                          depth: 20,
-                          lightSource: LightSource.topRight,
-                          shadowDarkColor: Colors.pink[50]),
-                      child: ClipOval(
-                        clipBehavior: Clip.hardEdge,
-                        child: CircleAvatar(
-                          child: Image.asset(
-                            'assets/avatar/img.jpg',
-                          ),
-                        ),
-                      )),
+                padding: EdgeInsets.only(left: 10),
+                height: AppBar().preferredSize.height,
+                // color: Colors.blueAccent,
+                child: Image(
+                  image: Image.asset('assets/avatar/logo.png').image,
                 ),
               ),
-              isScrolled? SizedBox(height :  2) : Text('Gaurav Anand', style: TextStyle(fontWeight: FontWeight.bold, color: Colors.white, fontSize: 18)),
+              Expanded(
+                child: Container(
+                  // color: (wi is BigAvatar) ? Colors.red : Colors.blue,
+                  margin: EdgeInsets.only(top: 25),
+                  // padding: EdgeInsets.only(top: 15),
+                  height: 180,
+                  child: AnimatedSwitcher(
+                    transitionBuilder: (widget, animation) => ScaleTransition(
+                      scale: animation,
+                      child: widget,
+                    ),
+                    child: wi,
+                    duration: Duration(milliseconds: 250),
+                  ),
+                ),
+              ),
             ],
           ),
         ),
@@ -61,68 +81,89 @@ class _HomePageState extends State<HomePage> {
             [
               Column(
                 children: [
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
-                  Text('Hi'),
                 ],
               ),
             ],
           ),
         ),
       ]),
+    );
+  }
+}
+
+class BigAvatar extends StatelessWidget {
+  const BigAvatar({
+    Key key,
+  }) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: EdgeInsets.only(top: 20),
+      child: Neumorphic(
+        padding: EdgeInsets.all(12),
+        style: NeumorphicStyle(
+            color: Color(0xffa1eafb),
+            boxShape: NeumorphicBoxShape.circle(),
+            depth: 20,
+            lightSource: LightSource.bottomRight,
+            shadowDarkColor: Color(0xffa1eafb)),
+        child: Neumorphic(
+            style: NeumorphicStyle(
+                boxShape: NeumorphicBoxShape.circle(),
+                depth: 20,
+                lightSource: LightSource.topLeft,
+                shadowDarkColor: Color(0xffffcef3)),
+            child: ClipOval(
+              clipBehavior: Clip.hardEdge,
+              child: CircleAvatar(
+                radius: 60,
+                child: Image.asset(
+                  'assets/avatar/img.jpg',
+                ),
+              ),
+            )),
+      ),
+    );
+  }
+}
+
+class SmallAvatar extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      padding: EdgeInsets.all(
+        4,
+      ),
+      margin: EdgeInsets.symmetric(
+        vertical: 1,
+      ),
+      child: Center(
+        child: Align(
+          alignment: Alignment.topRight,
+          child: Neumorphic(
+            style: NeumorphicStyle(
+              // border: NeumorphicBorder(
+              //   width: 5,
+              //   color: Color(0xffa1eafb)
+              // ),
+                boxShape: NeumorphicBoxShape.circle(),
+                depth: 20,
+                lightSource: LightSource.right,
+                shadowDarkColor: Color(0xffffcef3),
+                shadowLightColor: Colors.purple[100]),
+            child: CircleAvatar(
+              radius: AppBar().preferredSize.height / 2,
+              backgroundColor: Color(0xffa1eafb),
+              child: ClipOval(
+                child: Image(
+                  image: Image.asset('assets/avatar/img.jpg').image,
+                ),
+              ),
+            ),
+          ),
+        ),
+      ),
     );
   }
 }
