baf4613c24d3bcf2d336898a414a3510c915a7a6
pavankumarallu <vamsipavankumarallu@gmail.com>
2020-10-14 20:57:33 +0530

    Authentication pages added

--- app/gpa/lib/main.dart
+++ app/gpa/lib/main.dart
@@ -1,9 +1,16 @@
+import 'package:firebase_core/firebase_core.dart';
+import 'package:gpa/ui/pages/authentication.dart';
+import 'package:gpa/ui/pages/loginpage.dart';
+import 'package:provider/provider.dart';
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import './ui/pages/home.dart';
 import './ui/pages/result.dart';
 import 'package:flutter/services.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(MyApp());
 }
 
@@ -13,18 +20,36 @@ class MyApp extends StatelessWidget {
     SystemChrome.setSystemUIOverlayStyle(SystemUiOverlayStyle(
       statusBarColor: Color(0xffcabbf9), //or set color with: Color(0xFF0000FF)
     ));
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        primaryColor: Color(0xffcabbe9),
-        accentColor: Color(0xffffcef3),
+    return MultiProvider(
+      providers: [
+        Provider<AuthenticationService>(
+          create: (_) => AuthenticationService(FirebaseAuth.instance),
+        ),
+        StreamProvider(
+          create: (context) =>
+              context.read<AuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Flutter Demo',
+        theme: ThemeData(
+          primarySwatch: Colors.blue,
+          visualDensity: VisualDensity.adaptivePlatformDensity,
+        ),
+        home: AuthenticationWrapper(),
       ),
-      // home: HomePage(),
-      initialRoute: HomePage.id,
-      routes: {
-        ResultsPage.id: (context) => ResultsPage(),
-        HomePage.id: (context) => HomePage(),
-      },
     );
+  }
+}
+
+class AuthenticationWrapper extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    final firebaseUser = context.watch<User>();
+
+    if (firebaseUser != null) {
+      return HomePage();
+    }
+    return SignInPage();
   }
 }
