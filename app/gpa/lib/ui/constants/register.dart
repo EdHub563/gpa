7f125ad1102a37087553bea45bac0304916d98cb
aman440 <57002089+aman440@users.noreply.github.com>
2021-10-25 18:08:31 +0530

    Registration page added

---
+++ app/gpa/lib/ui/constants/register.dart
@@ -0,0 +1,238 @@
+//import 'dart:developer';
+
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:taskify/widgets/action.dart';
+import '../services/auth.dart';
+
+class RegisterPage extends StatefulWidget {
+  static String id = 'registerPage';
+  @override
+  _RegisterPageState createState() => _RegisterPageState();
+}
+
+class _RegisterPageState extends State<RegisterPage> {
+  TextEditingController _emailController = TextEditingController();
+  TextEditingController _passwordController = TextEditingController();
+  bool _showProgressBar = false;
+  Widget buildEmail() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Email',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6.0,
+                    offset: Offset(0, 2))
+              ]),
+          height: 60.0,
+          child: TextField(
+            controller: _emailController,
+            keyboardType: TextInputType.emailAddress,
+            style: TextStyle(
+              color: Colors.black,
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.mail,
+                  color: Color(0xff5ac18e),
+                ),
+                hintText: 'Enter Your Email id',
+                hintStyle: TextStyle(
+                  color: Colors.black38,
+                )),
+          ),
+        )
+      ],
+    );
+  }
+
+  Widget buildPassword() {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        Text(
+          'Password',
+          style: TextStyle(
+            color: Colors.white,
+            fontSize: 16.0,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        SizedBox(
+          height: 10,
+        ),
+        Container(
+          alignment: Alignment.centerLeft,
+          decoration: BoxDecoration(
+              color: Colors.white,
+              borderRadius: BorderRadius.circular(10.0),
+              boxShadow: [
+                BoxShadow(
+                    color: Colors.black26,
+                    blurRadius: 6.0,
+                    offset: Offset(0, 2))
+              ]),
+          height: 60.0,
+          child: TextField(
+            controller: _passwordController,
+            obscureText: true,
+            style: TextStyle(
+              color: Colors.black,
+            ),
+            decoration: InputDecoration(
+                border: InputBorder.none,
+                contentPadding: EdgeInsets.only(top: 14.0),
+                prefixIcon: Icon(
+                  Icons.lock,
+                  color: Color(0xff5ac18e),
+                ),
+                hintText: 'Enter Password',
+                hintStyle: TextStyle(
+                  color: Colors.black38,
+                )),
+          ),
+        )
+      ],
+    );
+  }
+
+  @override
+  void dispose() {
+    _emailController.dispose();
+    _passwordController.dispose();
+    super.dispose();
+  }
+
+  Widget buildSignUpBtn() {
+    return Container(
+      padding: EdgeInsets.symmetric(vertical: 25.0),
+      width: double.infinity,
+      child: RaisedButton(
+        elevation: 5,
+        onPressed: () async {
+          final auth = Provider.of<Auth>(context, listen: false);
+          setState(() {
+            _showProgressBar = true;
+          });
+          var result = await auth.register(
+              context: context,
+              email: _emailController.text.toString().trim(),
+              password: _passwordController.text.toString().trim());
+          setState(() {
+            _showProgressBar = false;
+          });
+          if (result)
+            print(result);
+          // Navigator.popAndPushNamed(context, HomePage.id);
+          else {
+            _passwordController.clear();
+          }
+        },
+        padding: EdgeInsets.all(15.0),
+        shape: RoundedRectangleBorder(
+          borderRadius: BorderRadius.circular(15),
+        ),
+        color: Colors.white,
+        child: Text(
+          'SIGN UP',
+          style: TextStyle(
+            color: Color(0xff5ac18e),
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+      ),
+    );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: AnnotatedRegion<SystemUiOverlayStyle>(
+        value: SystemUiOverlayStyle.light,
+        child: GestureDetector(
+          child: Stack(
+            children: [
+              Container(
+                  height: double.infinity,
+                  width: double.infinity,
+                  decoration: BoxDecoration(
+                    image: DecorationImage(
+                      image: Image.asset('assets/images/bg.jpg').image,
+                      colorFilter: new ColorFilter.mode(
+                          Colors.black.withOpacity(0.3), BlendMode.dstATop),
+                      fit: BoxFit.cover,
+                    ),
+                    gradient: LinearGradient(
+                        begin: Alignment.topCenter,
+                        end: Alignment.bottomCenter,
+                        colors: [
+                          Color(0x665ac18e),
+                          Color(0x995ac18e),
+                          Color(0xcc5ac18e),
+                          Color(0xff5ac18e),
+                        ]),
+                  ),
+                  child: SingleChildScrollView(
+                    physics: AlwaysScrollableScrollPhysics(),
+                    padding:
+                        EdgeInsets.symmetric(horizontal: 25.0, vertical: 120.0),
+                    child: Column(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: [
+                        Text(
+                          'Sign Up',
+                          style: TextStyle(
+                            color: Colors.white,
+                            fontSize: 40.0,
+                            fontWeight: FontWeight.bold,
+                          ),
+                        ),
+                        SizedBox(
+                          height: 50,
+                        ),
+                        buildEmail(),
+                        SizedBox(
+                          height: 20,
+                        ),
+                        buildPassword(),
+                        buildSignUpBtn(),
+                      ],
+                    ),
+                  )),
+              Padding(
+                  padding: EdgeInsets.only(left: 20, top: 40),
+                  child: ActionButton(
+                    iconData: CupertinoIcons.left_chevron,
+                    onTap: () {
+                      Navigator.pop(context);
+                    },
+                  )),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
